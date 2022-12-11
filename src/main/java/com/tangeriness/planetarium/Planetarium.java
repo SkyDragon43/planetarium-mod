1c955156dc16d3c2ef0a2bf9c8160eb65cecd9c1
SkyDragon43 <65665302+SkyDragon43@users.noreply.github.com>
2022-12-10 21:17:51 -0500

    progress!
    yay

--- src/main/java/com/tangeriness/planetarium/Planetarium.java
+++ src/main/java/com/tangeriness/planetarium/Planetarium.java
@@ -1,5 +1,19 @@
 package com.tangeriness.planetarium;
 
-public class Planetarium {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Planetarium implements ModInitializer{
+    public static final String MOD_ID = "planetarium";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info(MOD_ID + " INITEITLIZEDDSD SUCCESS!!!!!!!!!!!!!!!!!!");
+    }
     
 }
